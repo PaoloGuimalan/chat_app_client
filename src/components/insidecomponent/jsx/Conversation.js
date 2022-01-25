4b3d217e7251157c43b6c1b59eaf8d3a83158bf7
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-01-25 08:33:06 -0800

    ChatApp w/ MongoDB

--- src/components/insidecomponent/jsx/Conversation.js
+++ src/components/insidecomponent/jsx/Conversation.js
@@ -5,7 +5,8 @@ import '../css/Conversation.css'
 import Axios from 'axios';
 import {motion} from 'framer-motion';
 import imgperson from '../../imgs/person-icon.png';
-import ScrollToBottom from 'react-scroll-to-bottom'
+import ScrollToBottom from 'react-scroll-to-bottom';
+import audiomes from '../../../sounds/bbm_tone.mp3'
 
 function Conversation ({user}) {
     const { conid } = useParams();
@@ -16,6 +17,8 @@ function Conversation ({user}) {
     const [Recc, setRec] = useState(user == conid.split("&")[1]? conid.split("&")[0] : conid.split("&")[1]);
     const [conid_ver, setconid_ver] = useState("");
     const [rdr, setRdr] = useState(false);
+    const [mymes, setmymes] = useState(0);
+    const [sendermes, setsendermes] = useState(0);
 
     useEffect(async () => {
         await Axios.get(`https://chatappnode187.herokuapp.com/conversation/${conid}`).then( async (response) => {
@@ -27,6 +30,10 @@ function Conversation ({user}) {
                 await setRec(user == conid.split("&")[1]? conid.split("&")[0] : conid.split("&")[1]);
                 //setconid_ver(convo.conversation_id[0]);
                 await setconid_ver(made_id === "" ? conid : made_id);
+                const length_one = await response.data.filter((count) => count.who_sent == user ? (count.who_sent) : "");
+                const length_two = await response.data.filter((count) => count.who_sent != user ? (count.who_sent) : "");
+                setmymes(length_one.length);
+                setsendermes(length_two.length)
                 //console.log(conid_ver);
                 //console.log(made_id === "" ? conid : made_id)
             //console.log(reversed);
@@ -37,15 +44,25 @@ function Conversation ({user}) {
       setRdr(false);
       setTimeout(() => {
           setRdr(true);
-          window.scrollTo(0,document.body.scrollHeight);
+        //   window.scrollTo(0,document.body.scrollHeight);
+        // scroller();
       }, 2000)
     }, [conid]);
     
+    useEffect(() => {
+      scroller();
+    }, [mymes]);
+    
+    useEffect(() => {
+        let audioMessage = new Audio(audiomes);
+        audioMessage.play();
+    }, [sendermes]);
     
 
     const send_provider = async () => {
         //alert(Recc);
         setTxt("");
+        // scroller();
         await Axios.post('https://chatappnode187.herokuapp.com/sendto', {
             id: conid_ver,
             txt: txt,
@@ -53,7 +70,6 @@ function Conversation ({user}) {
             to: Recc
         }).then((response) => {
             console.log("Okay");
-            scroller();
         }).catch((err) => {
             console.log(err);
         });
