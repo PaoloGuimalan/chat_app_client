ce6e5fe6e5f3eabee5fbab109cd87bb2bb9248e8
PaoloGuimalan <pauloportes.guimalan187@gmail.com>
2022-02-13 09:32:51 -0800

    Conversation User Status Added

--- src/socket/clientsocket.js
+++ src/socket/clientsocket.js
@@ -1,14 +1,17 @@
 // console.log("Hello!");
 const io = require("socket.io-client");
-const socket = io.connect("https://chatappsocket187.herokuapp.com/", {
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    },
-    'sync disconnect on unload': true, transports : ['websocket'] });
+// const socket = io.connect("localhost:3002/", {
+//     withCredentials: true,
+//     extraHeaders: {
+//       "my-custom-header": "abcd"
+//     },
+//     'sync disconnect on unload': true, transports : ['websocket'] });
+
+const socket = io.connect("https://chatappsocket187.herokuapp.com/");
 
 socket.on("connect", () => {
     console.log("Connected!");
     socket.emit("connected");
-    socket.emit("disconnect")
+    socket.emit("disconnect");
+    // socket.disconnect();
 })
