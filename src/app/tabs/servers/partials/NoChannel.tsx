af94b786fbae0c38f767f08879b1f3c0352f8803
Paolo <pauloportes.guimalan187@gmail.com>
2024-03-14 13:16:09 +0800

    fix: no channel label has been modified for temporary use

--- src/app/tabs/servers/partials/NoChannel.tsx
+++ src/app/tabs/servers/partials/NoChannel.tsx
@@ -1,8 +1,8 @@
 
 function NoChannel() {
   return (
-    <div className="tw-bg-transparent tw-flex tw-flex-1 tw-flex-row tw-items-center tw-justify-center">
-        Select a channel
+    <div className="tw-bg-[#f1f1f2] tw-h-full tw-flex tw-flex-1 tw-flex-row tw-items-center tw-justify-center">
+        <span className="tw-text-[13px]">Select a channel</span>
     </div>
   )
 }
