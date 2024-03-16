f6c5a34991ace3a9739e673ca444a6c7fe11a72d
Paolo <pauloportes.guimalan187@gmail.com>
2024-03-16 22:16:25 +0800

    feat: mobile scaled splash screen has been implemented

--- src/app/main/Splash.tsx
+++ src/app/main/Splash.tsx
@@ -1,11 +1,15 @@
 import '../../styles/styles.css'
 import ChatterLoopImg from '../../assets/imgs/chatterloop.png'
 import { motion } from 'framer-motion'
+import { useSelector } from 'react-redux';
 
 function Splash() {
+
+  const screensizelistener = useSelector((state: any) => state.screensizelistener);
+
   return (
     <div id='div_splash'>
-      <div id='div_icon_container'>
+      <div id={screensizelistener.W <= 900 ? 'div_icon_container_m' : 'div_icon_container'}>
         <motion.img
         animate={{
           scale: 1.1
@@ -17,8 +21,8 @@ function Splash() {
         src={ChatterLoopImg} id='img_icon_splash' />
       </div>
       <div id='div_icon_labels'>
-        <span className='span_icon_label'>Chatterloop</span>
-        <span className='span_icon_label'>Link . Share . Explore</span>
+        <span className={screensizelistener.W <= 900 ? 'span_icon_label_m' : 'span_icon_label'}>Chatterloop</span>
+        <span className={screensizelistener.W <= 900 ? 'span_icon_label_m' : 'span_icon_label'}>Link . Share . Explore</span>
       </div>
     </div>
   )
