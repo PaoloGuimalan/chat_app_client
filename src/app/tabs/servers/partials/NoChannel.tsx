35f5659281fc440163ca92869a323c35887d3580
Paolo <pauloportes.guimalan187@gmail.com>
2025-11-26 04:28:30 +0800

    feat: removed no channel and server conversation background svg temporarily

--- src/app/tabs/servers/partials/NoChannel.tsx
+++ src/app/tabs/servers/partials/NoChannel.tsx
@@ -1,12 +1,14 @@
-
 function NoChannel() {
   return (
     <div className="tw-bg-[#f1f1f2] tw-h-full tw-flex tw-flex-1 tw-flex-row tw-items-center tw-justify-center">
-        <div id="div_server_conversation_list" className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full">
-            <span className="tw-text-[13px]">Select a channel</span>
-        </div>
+      <div
+        // id="div_server_conversation_list"
+        className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full"
+      >
+        <span className="tw-text-[13px]">Select a channel</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NoChannel
+export default NoChannel;
--- src/app/tabs/servers/partials/ServerConversation.tsx
+++ src/app/tabs/servers/partials/ServerConversation.tsx
@@ -10,74 +10,86 @@ import Conversation from "../../messenger/Conversation";
 import { motion } from "framer-motion";
 
 function ServerConversation() {
-    
-  const screensizelistener = useSelector((state: any) => state.screensizelistener);
-  const conversationsetup = useSelector((state: any) => state.conversationsetup);
+  const screensizelistener = useSelector(
+    (state: any) => state.screensizelistener
+  );
+  const conversationsetup = useSelector(
+    (state: any) => state.conversationsetup
+  );
   const params = useParams();
   const dispatch = useDispatch();
 
-  const [isconversationsetuploaded, setisconversationsetuploaded] = useState<boolean>(false);
+  const [isconversationsetuploaded, setisconversationsetuploaded] =
+    useState<boolean>(false);
   const conversationID = useMemo(() => params.conversationID, [params]);
 
   useEffect(() => {
     setisconversationsetuploaded(false);
     InitServerConversationRequest({
-        conversationID: conversationID
-    }).then((response) => {
-        const conversationsetupresponse = {
-            conversationid: response[0].conversationID,
-            userdetails: conversationsetupstate.userdetails,
-            groupdetails: {
-                ...response[0].groupdetails,
-                receivers: response[0].receivers
-            },
-            type: "server"
-        }
-        dispatch({
-            type: SET_CONVERSATION_SETUP,
-            payload:{
-                conversationsetup: conversationsetupresponse
-            }
-        })
-        setisconversationsetuploaded(true);
-    }).catch((err) => {
-        console.log(err);
+      conversationID: conversationID,
     })
-  },[conversationID]);
+      .then((response) => {
+        const conversationsetupresponse = {
+          conversationid: response[0].conversationID,
+          userdetails: conversationsetupstate.userdetails,
+          groupdetails: {
+            ...response[0].groupdetails,
+            receivers: response[0].receivers,
+          },
+          type: "server",
+        };
+        dispatch({
+          type: SET_CONVERSATION_SETUP,
+          payload: {
+            conversationsetup: conversationsetupresponse,
+          },
+        });
+        setisconversationsetuploaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [conversationID]);
 
   useEffect(() => {
-
     return () => {
-        dispatch({
-            type: SET_CONVERSATION_SETUP,
-            payload:{
-                conversationsetup: conversationsetupstate
-            }
-        })
-    }
-  },[])
+      dispatch({
+        type: SET_CONVERSATION_SETUP,
+        payload: {
+          conversationsetup: conversationsetupstate,
+        },
+      });
+    };
+  }, []);
 
   return (
     <motion.div
-    initial={{
-       borderBottomLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
-       borderTopLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px"
-    }}
-    animate={{
+      initial={{
         borderBottomLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
-        borderTopLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px"
-    }}
-    id="div_server_conversation_list"
-    className="tw-bg-[#f1f1f2] tw-flex tw-flex-col tw-flex-1 tw-items-center tw-justify-center tw-h-full tw-rounded-tr-[10px] tw-rounded-br-[10px]">
-        {conversationsetup.conversationid && isconversationsetuploaded ? (
-            <Conversation conversationsetup={conversationsetup} theme={{ primary: "#e69500", lighten: "#ffc965" }} />
-        ) : (
-            <div id="div_server_conversation_list" className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full">
-                <span className="tw-text-[13px]">Loading...</span>
-            </div>
-        )}
+        borderTopLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
+      }}
+      animate={{
+        borderBottomLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
+        borderTopLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
+      }}
+      // id="div_server_conversation_list"
+      className="tw-bg-[#f1f1f2] tw-flex tw-flex-col tw-flex-1 tw-items-center tw-justify-center tw-h-full tw-rounded-tr-[10px] tw-rounded-br-[10px]"
+    >
+      {conversationsetup.conversationid && isconversationsetuploaded ? (
+        <Conversation
+          conversationsetup={conversationsetup}
+          theme={{ primary: "#e69500", lighten: "#ffc965" }}
+        />
+      ) : (
+        <div
+          // id="div_server_conversation_list"
+          className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full"
+        >
+          <span className="tw-text-[13px]">Loading...</span>
+        </div>
+      )}
     </motion.div>
-  )
+  );
 }
 
-export default ServerConversation
+export default ServerConversation;
