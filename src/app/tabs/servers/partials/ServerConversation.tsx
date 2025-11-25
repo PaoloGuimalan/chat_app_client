f789c50fabd12401c5ec2cb18301674a43a5d05d
John Paulo Guimalan <john.guimalan@RD-JGUIMALAN-MNL.local>
2025-11-26 00:21:19 +0800

    feat: changed svg background for server conversation and no channel panel

--- src/app/tabs/servers/partials/ServerConversation.tsx
+++ src/app/tabs/servers/partials/ServerConversation.tsx
@@ -67,12 +67,12 @@ function ServerConversation() {
         borderBottomLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px",
         borderTopLeftRadius: screensizelistener.W <= 900 ? "10px" : "0px"
     }}
-    id="div_server_list"
+    id="div_server_conversation_list"
     className="tw-bg-[#f1f1f2] tw-flex tw-flex-col tw-flex-1 tw-items-center tw-justify-center tw-h-full tw-rounded-tr-[10px] tw-rounded-br-[10px]">
         {conversationsetup.conversationid && isconversationsetuploaded ? (
             <Conversation conversationsetup={conversationsetup} theme={{ primary: "#e69500", lighten: "#ffc965" }} />
         ) : (
-            <div id="div_server_list" className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full">
+            <div id="div_server_conversation_list" className="tw-rounded-[10px] tw-bg-white tw-flex tw-items-center tw-justify-center tw-w-full tw-h-full">
                 <span className="tw-text-[13px]">Loading...</span>
             </div>
         )}
