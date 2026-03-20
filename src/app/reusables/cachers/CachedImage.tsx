521449f760bcf155691ce8b68291398341d22a41
Paolo <pauloportes.guimalan187@gmail.com>
2026-03-20 18:31:44 +0800

    fix: temporarily disabled image caching for memory blockage prevention

--- src/app/reusables/cachers/CachedImage.tsx
+++ src/app/reusables/cachers/CachedImage.tsx
@@ -1,30 +1,46 @@
-import { persistAndReturnImage } from "@/reusables/hooks/localforagehelper";
+// import { persistAndReturnImage } from "@/reusables/hooks/localforagehelper";
 import { CachedImageProp } from "@/reusables/vars/props";
 import { useEffect, useState } from "react";
 
 function CachedImage({ src, className, id, onLoad, onClick }: CachedImageProp) {
   const [workingURL, setworkingURL] = useState<string | null | undefined>(null);
+  const [onError, setonError] = useState<boolean>(false);
 
   useEffect(() => {
-    persistAndReturnImage("resources", src!)
-      .then((value: string) => {
-        setworkingURL(value);
-      })
-      .catch(() => {
-        setworkingURL(src);
-      });
+    setworkingURL(src);
+    // persistAndReturnImage("resources", src!)
+    //   .then((value: string) => {
+    //     setworkingURL(value);
+    //   })
+    //   .catch(() => {
+    //     setworkingURL(src);
+    //   });
   }, [src]);
 
   return (
-    workingURL && (
+    workingURL &&
+    (onError ? (
+      <div
+        id={id}
+        title="Tap to Refresh"
+        className={`${className} img-placeholder`}
+        onLoad={onLoad}
+        onClick={() => {
+          setonError(false);
+        }}
+      />
+    ) : (
       <img
         id={id}
         className={className}
         src={workingURL}
         onLoad={onLoad}
         onClick={onClick}
+        onError={() => {
+          setonError(true);
+        }}
       />
-    )
+    ))
   );
 }
 
